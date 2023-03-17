300745b2e9bf7010bfa9f747bf5f3f68e21e26f6
teruhito <ataeteruhito@gmail.com>
2023-03-17 23:02:59 +0900

    installのみで #80

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,8 +4,7 @@
 #  TapDrillForKids
 #
 #  Created by 若江照仁 on 2023/03/17.
-#  
-cd $CI_WORKSPACE
+#
 
 # SwiftGen
-xcodebuild -workspace TapDrillForKids.xcworkspace -scheme swift-gen
+brew install swiftgen
