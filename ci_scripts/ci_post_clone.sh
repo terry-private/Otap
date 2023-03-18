439c3ba5706f0c9d73235e008b908c104aa2deb0
teruhito <ataeteruhito@gmail.com>
2023-03-18 09:06:34 +0900

    Revert "全path指定 #80"
    This reverts commit 9c61e87bbf190d20427823d3ca69a245648bc272.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,13 +6,5 @@
 #  Created by 若江照仁 on 2023/03/17.
 #
 
-cd $CI_WORKSPACE
-
 # SwiftGen
 brew install swiftgen
-swiftgen config run --config "$CI_WORKSPACE/Modules/Sources/Drills/swiftgen.yml"
-swiftgen config run --config "$CI_WORKSPACE/Modules/Sources/FeatureDrill/swiftgen.yml"
-swiftgen config run --config "$CI_WORKSPACE/Modules/Sources/FeatureMainTab/swiftgen.yml"
-swiftgen config run --config "$CI_WORKSPACE/Modules/Sources/FeatureSelectDrill/swiftgen.yml"
-swiftgen config run --config "$CI_WORKSPACE/Modules/Sources/FeatureSelectLevel/swiftgen.yml"
-swiftgen config run --config "$CI_WORKSPACE/Modules/Sources/FeatureSetting/swiftgen.yml"
